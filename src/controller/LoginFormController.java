4e4f8488a18fc8d105a9d2efd42e946880c124cc
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2022-08-15 18:08:54 +0530

    Added AppInitializer

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,10 +2,35 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public JFXTextField txtUserName;
+    public static String userName;
 
-    public void btnLoginOnAction(ActionEvent actionEvent) {
+    public void btnLoginOnAction(ActionEvent actionEvent) throws IOException {
+        if (txtUserName.getText().equals("Client1")){
+            Stage stage = (Stage) txtUserName.getScene().getWindow();
+            stage.close();
+            Stage stage1 = new Stage();
+            stage1.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/Client1Form.fxml"))));
+            stage1.setTitle("Online Chat ");
+            stage1.setResizable(false);
+            stage1.centerOnScreen();
+            stage1.show();
+        }
+
+        else if (txtUserName.getText().equals("Server")) {
+            Stage stage2 = new Stage();
+            stage2.setScene(new Scene(FXMLLoader.load(getClass().getResource("/view/ServerForm.fxml"))));
+            stage2.setTitle("Online Chat");
+            stage2.setResizable(false);
+            stage2.centerOnScreen();
+            stage2.show();
+        }
     }
 }
