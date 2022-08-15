49bdc2e11759d0a1736209d4e6a8175c849a0808
Lasari Imalsha <101311493+LasariImalsha@users.noreply.github.com>
2022-08-15 17:40:39 +0530

    Added view Forms

--- src/controller/Client1FormController.java
+++ src/controller/Client1FormController.java
@@ -1,4 +1,19 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+
 public class Client1FormController {
+    public Label lblUser;
+    public JFXTextField txtTextField;
+    public VBox vBox;
+    public ImageView imgSendImages;
+    public Pane emojiPane;
+
+    public void sendOnAction(ActionEvent actionEvent) {
+    }
 }
--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -1,4 +1,11 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
 public class LoginFormController {
+    public JFXTextField txtUserName;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+    }
 }
